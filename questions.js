161e98ea684c4e1b887b88eb6a21a3eafc56bc30
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-08-23 16:05:03 -0400

    navbar fixed

--- questions.js
+++ questions.js
@@ -4,45 +4,45 @@ var quizDuration = 60;
 
 var questions = [
 	{
-	    name:"Commonly used types DO NOT include:",
+	    name:"HTML stands for :",
 	    answers:[
 		    	{ 
 		    		id: 123, 
-		    		name: "Strings"
+		    		name: "Hyper Transient Maker Language"
 		    	},
 		    	{ 
 		    		id: 234, 
-		    		name: "Alerts"
+		    		name: "Hyper Text Markup Language"
 		    	},
 		    	{ 
 		    		id: 567, 
-		    		name: "Numbers"
+		    		name: "Halode Typographic Maximized Lexicon"
 		    	},
 		    	{ 
 		    		id: 78, 
-		    		name: "Booleans"
+		    		name: "Hadron Transforming Markdown Launcher"
 		    	}
 	    ],
 	    rightAnswer:234
 	},
 	{
-	    name:"The condition in an if / else statement is enclosed within___.",
+	    name:"The following is not an array method: ",
 	    answers:[
 		    	{ 
 		    		id: 897, 
-		    		name: "Curly brackets"
+		    		name: "array.splice()"
 		    	},
 		    	{ 
 		    		id: 9098, 
-		    		name: "Quotes"
+		    		name: "array.slice()"
 		    	},
 		    	{ 
 		    		id: 98754, 
-		    		name: "Parentheses"
+		    		name: "array.spin()"
 		    	},
 		    	{ 
 		    		id: 1231, 
-		    		name: "Square brackets"
+		    		name: "array.split()"
 		    	}
 	    ],
 	    rightAnswer:98754
@@ -67,7 +67,7 @@ var questions = [
 		    		name: "All of the above"
 		    	}
 	    ],
-	    rightAnswer: 555
+	    rightAnswer: 5555
 	},
 	{
 	    name:"String values must be enclosed within___when being assigned to variables.",
@@ -90,5 +90,28 @@ var questions = [
 		    	}
 	    ],
 	    rightAnswer:8888
+	},
+	{
+	    name:"If (i != 0) runs when",
+	    answers:[
+		    	{ 
+		    		id: 594, 
+		    		name: "i is not equal to 0"
+		    	},
+		    	{ 
+		    		id: 2255, 
+		    		name: "i is equal to 0"
+		    	},
+		    	{ 
+		    		id: 1195, 
+		    		name: "i = zero"
+		    	},
+		    	{ 
+		    		id: 5592, 
+		    		name: "i is undefined"
+		    	}
+	    ],
+	    rightAnswer:594
 	}
+
 ];
